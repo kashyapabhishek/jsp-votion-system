6c5dce69ffe3aa0abe14b534b2a330e446e7149f
abhishek kashyap <kashyap638@gmail.com>
2018-03-04 21:10:24 +0530

    userbean class add

---
+++ src/com/bean/UserBean.java
@@ -0,0 +1,29 @@
+package com.bean;
+
+public class UserBean {
+	private String email, vote, timeanddate;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getVote() {
+		return vote;
+	}
+
+	public void setVote(String vote) {
+		this.vote = vote;
+	}
+
+	public String getTimeanddate() {
+		return timeanddate;
+	}
+
+	public void setTimeanddate(String timeanddate) {
+		this.timeanddate = timeanddate;
+	}
+}
